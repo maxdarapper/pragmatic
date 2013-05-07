48ca2e68a5bc133b934a1b995658ccdca75aa203
max na <max@weight.com>
2013-05-06 19:19:31 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Events::Application.config.secret_token = '6f32e30a65ca1a70f5dc3d1029772e872ad608a8a03d1035f2d242545b8ffe5ad149633b3d60bef7877a71d07a5ba3a37201bc0a6dee507d067c41eaad7efbba'
